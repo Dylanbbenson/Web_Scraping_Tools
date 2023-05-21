e44feaa857bbdd691fc8c8877c7e2d419ec20585
Dylan Benson <dylanbenson@tutanota.com>
2023-05-20 21:13:28 -0500

    Updated script to fix bugs

--- zillow_data_scrape.py
+++ zillow_data_scrape.py
@@ -8,7 +8,6 @@ from datetime import date
 warnings.filterwarnings('ignore')
 
 try:
-    
     # take in command line arguments
     city = sys.argv[1]
     state = sys.argv[2]
@@ -32,7 +31,7 @@ try:
     # grab the first 20 pages
     base_url = 'https://www.zillow.com/homes/for_sale/{}/{}_p/'
     urls = [base_url.format(city, i) for i in range(1, 11)]
-    base_url2 = 'https://www.zillow.com/{}/apartments/'
+    base_url2 = 'https://www.zillow.com/{}/apartments/{}_p/'
     urls2 = [base_url2.format(city_state, i) for i in range(1, 11)]
 
     print("Scraping data...")
@@ -51,12 +50,16 @@ try:
         data_list = []
         for url in urls:
             r = s.get(url, headers=req_headers)
-            data_list.append(json.loads(re.search(r'!--(\{"queryState".*?)-->', r.text).group(1)))
+            match = re.search(r'!--(\{"queryState".*?)-->', r.text)
+            if match:
+                data_list.append(json.loads(match.group(1)))
 
         data_list2 = []
         for url2 in urls2:
             r = s.get(url2, headers=req_headers)
-            data_list2.append(json.loads(re.search(r'!--(\{"queryState".*?)-->', r.text).group(1)))
+            match2 = re.search(r'!--(\{"queryState".*?)-->', r.text)
+            if match2:
+                data_list2.append(json.loads(match2.group(1)))
 
     df = pd.DataFrame()
     df2 = pd.DataFrame()
@@ -77,20 +80,44 @@ try:
 
     df['zestimate'] = df['zestimate'].fillna(0)
     df['best_deal'] = df['unformattedPrice'] - df['zestimate']
-    df = df.sort_values(by='best_deal', ascending=True)
+    df = df.sort_values(by='unformattedPrice', ascending=True)
+    df2 = df2.sort_values(by='unformattedPrice', ascending=True)
+    null_price = df[df["unformattedPrice"].isnull()]
+    df.drop(null_price.index, inplace=True)
+    ############################################################################################################
 
-    df2['zestimate'] = df2['zestimate'].fillna(0)
-    df2['best_deal'] = df2['unformattedPrice'] - df2['zestimate']
-    df2 = df2.sort_values(by='best_deal', ascending=True)
+    pattern = r"\$(\d+(?:,\d+)?)\+"
+    df2['units'] = df2['units'].astype(str)
+    def extract_price(text):
+        match = re.search(pattern, text)
+        if match:
+            return match.group(1)
+        else:
+            return None
+
+    df2["units"] = df2["units"].apply(extract_price)
+    df2['unformattedPrice'].fillna(df2['units'], inplace=True)
+    null_price = df2['unformattedPrice'].isnull()
+    not_null_units = ~df2['units'].isnull()
+    df2.loc[null_price & not_null_units, 'unformattedPrice'] = df2.loc[null_price & not_null_units, 'units']
+    null_price2 = df2[df2["unformattedPrice"].isnull()]
+    df2.drop(null_price2.index, inplace=True)
+    df2['unformattedPrice'] = pd.to_numeric(df2['unformattedPrice'], errors='coerce')
+    df2['price'] = '$' + df2['unformattedPrice'].astype(str) + '/mo'
+    df2 = df2.dropna(subset=['unformattedPrice'])
+    df2 = df2.sort_values('unformattedPrice')
+
+    ############################################################################################################
 
     # select certain fields and output (this portion can be changed)
     data = df[['zpid', 'imgSrc', 'statusType', 'price', 'unformattedPrice', 'zestimate', 'best_deal', 'address', 'addressZipcode', 'beds', 'baths', 'area', 'variableData', 'brokerName', 'builderName']]
     data.to_csv("./data/" + city + "_Homes_ForSale_"+current_date+".csv")
 
-    data2 = df2[['zpid', 'imgSrc', 'statusType', 'price', 'unformattedPrice', 'zestimate', 'best_deal', 'address', 'addressZipcode', 'beds', 'baths', 'area', 'variableData']]
+    data2 = df2[['zpid', 'imgSrc', 'statusType', 'price', 'unformattedPrice', 'address', 'addressZipcode', 'beds', 'baths', 'area', 'variableData']]
     data2.to_csv("./data/" + city + "_Apartments_ForRental_"+current_date+".csv")
 
     print('Done.\n'+city+"_Homes_ForSale_"+current_date+".csv is available for viewing.\n"+city+"_Apartments_ForRental_"+current_date+".csv is available for viewing.")
+
 
 except:
     print("Error: The City or State entered could not be found.")
